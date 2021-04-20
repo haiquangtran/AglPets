d187b43990821af7188a8dfad5829b06ff8ad814
haiquangtran <hai.quang.tran@gmail.com>
2021-04-21 03:05:55 +1200

    Print out cats in ConsoleApp.

---
+++ Agl.Pets.ConsoleApp/Plumbing/AppModules.cs
@@ -0,0 +1,32 @@
+﻿using Agl.Pets.Core.Api;
+using Agl.Pets.Infrastructure.PetOwners;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agl.Pets.ConsoleApp.Plumbing
+{
+    [ExcludeFromCodeCoverage]
+    public static class AppModules
+    {
+        public static IServiceCollection ConfigureAppServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddLogging();
+            services.AddOptions();
+            services.AddHttpClient();
+
+            // Add Pet Owner api
+
+            services.AddPetOwnerApi(configuration.GetSection("PetOwner"));
+
+            return services;
+        }
+    }
+}
--- Agl.Pets.ConsoleApp/Program.cs
+++ Agl.Pets.ConsoleApp/Program.cs
@@ -1,12 +1,56 @@
-﻿using System;
+﻿using Agl.Pets.ConsoleApp.Cats;
+using Agl.Pets.ConsoleApp.Plumbing;
+using Agl.Pets.Infrastructure.PetOwners;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
 
 namespace Agl.Pets.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Enable App settings and configuration
+            
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            IConfiguration configuration = new ConfigurationBuilder()
+              .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+              .AddJsonFile($"appsettings.{env}.json", true, true)
+              .AddEnvironmentVariables()
+              .Build();
+
+            using IHost host = CreateHostBuilder(args, configuration).Build();
+
+            // Print cats
+
+            try
+            {
+                Console.WriteLine("Loading cat pet owners...Please wait");
+
+                using IServiceScope serviceScope = host.Services.CreateScope();
+                IServiceProvider provider = serviceScope.ServiceProvider;
+                IPetOwnerHttpClient petOwnerClient = provider.GetRequiredService<IPetOwnerHttpClient>();
+
+                var petOwners = await petOwnerClient.GetPetOwners();
+
+                // Print Owners and Cats
+
+                CatPrinter.PrintOwnersAndCats(petOwners);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Oops, soemthing went wrong, please try again. {ex.Message}");
+
+                throw;
+            }
+
+            await host.RunAsync();
         }
+
+        static IHostBuilder CreateHostBuilder(string[] args, IConfiguration configuration) => Host.CreateDefaultBuilder(args)
+              .ConfigureServices((_, services) => services.ConfigureAppServices(configuration));
     }
 }
