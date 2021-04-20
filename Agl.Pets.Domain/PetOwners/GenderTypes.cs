5168936efe910ead564c6dfa18cb5543e2903544
haiquangtran <hai.quang.tran@gmail.com>
2021-04-21 03:05:09 +1200

    Added types in Domain layer.

---
+++ Agl.Pets.Domain/PetOwners/GenderTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agl.Pets.Domain.PetOwners
+{
+    public static class GenderTypes
+    {
+        public const string Male = "Male";
+        public const string Female = "Female";
+    }
+}
